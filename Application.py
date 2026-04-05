2d7d60ab0fb8d791ebcfecd217cfb5d181b0c8f4
fengleng-yorku <felixrei@my.yorku.ca>
2026-04-05 10:26:06 -0400

    Fix Flask host binding to 0.0.0.0

--- Application.py
+++ Application.py
@@ -41,4 +41,4 @@ def get_listStudentsByCourseID_response():
     return jsonify({"response": listOfStudents})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
